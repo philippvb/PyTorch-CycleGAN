546bbed59de6dc370c9b1e75291001f30b8e0f0b
Philipp von Bachmann <philippvonbachmann@web.de>
2022-05-10 18:02:15 +0200

    Added replaybuffer again

--- utils.py
+++ utils.py
@@ -5,6 +5,7 @@ import sys
 
 from torch.autograd import Variable
 import torch
+# from visdom import Visdom
 import numpy as np
 
 def tensor2image(tensor):
@@ -12,7 +13,27 @@ def tensor2image(tensor):
     if image.shape[0] == 1:
         image = np.tile(image, (3,1,1))
     return image.astype(np.uint8)
+class ReplayBuffer():
+    def __init__(self, max_size=50):
+        assert (max_size > 0), 'Empty buffer or trying to create a black hole. Be careful.'
+        self.max_size = max_size
+        self.data = []
 
+    def push_and_pop(self, data):
+        to_return = []
+        for element in data.data:
+            element = torch.unsqueeze(element, 0)
+            if len(self.data) < self.max_size:
+                self.data.append(element)
+                to_return.append(element)
+            else:
+                if random.uniform(0,1) > 0.5:
+                    i = random.randint(0, self.max_size-1)
+                    to_return.append(self.data[i].clone())
+                    self.data[i] = element
+                else:
+                    to_return.append(element)
+        return Variable(torch.cat(to_return))
 
 class LambdaLR():
     def __init__(self, n_epochs, offset, decay_start_epoch):
