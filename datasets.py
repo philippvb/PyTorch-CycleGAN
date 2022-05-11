13989fa1bf3df6c60f24493c2212fc513a5c9c56
Philipp von Bachmann <philippvonbachmann@web.de>
2022-05-11 10:32:01 +0200

    Added skipping of grayscale images to dataset

--- datasets.py
+++ datasets.py
@@ -6,6 +6,7 @@ from torch.utils.data import Dataset
 from PIL import Image
 import torchvision.transforms as transforms
 import torch
+import numpy as np
 
 class ImageDataset(Dataset):
     def __init__(self, root, transforms_=None, unaligned=False, mode='train'):
@@ -38,19 +39,29 @@ class RamImageDataset(Dataset):
             self.tensors_A = []
             self.tensors_B = []
             for filename_A in self.files_A:
-                self.tensors_A.append(Image.open(filename_A))
+                im = Image.open(filename_A)
+                # only append if right shape
+                if np.array(im).shape == (256, 256, 3):
+                    self.tensors_A.append(im)
+                else:
+                    print(f"WARNING: The image {filename_A} seems to be grayscale, thus skipping.")
             for filename_B in self.files_B:
-                self.tensors_B.append(Image.open(filename_B))
+                im = Image.open(filename_B)
+                # only append if right shape
+                if np.array(im).shape == (256, 256, 3):
+                    self.tensors_B.append(im)
+                else:
+                    print(f"WARNING: The image {filename_B} seems to be grayscale, thus skipping.")
 
         def __getitem__(self, index):
-            item_A = self.transform(self.tensors_A[index % len(self.files_A)])
+            item_A = self.transform(self.tensors_A[index % len(self.tensors_A)])
 
             if self.unaligned:
-                item_B = self.transform(self.tensors_B[random.randint(0, len(self.files_B) - 1)])
+                item_B = self.transform(self.tensors_B[random.randint(0, len(self.tensors_B) - 1)])
             else:
-                item_B = self.transform(self.tensors_B[index % len(self.files_B)])
+                item_B = self.transform(self.tensors_B[index % len(self.tensors_B)])
 
             return {'A': item_A, 'B': item_B}
 
         def __len__(self):
-            return max(len(self.files_A), len(self.files_B))
+            return max(len(self.tensors_A), len(self.tensors_B))
