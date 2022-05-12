9711657d7d6b98d3c3b06bebfbec6d0ff25a8afb
Philipp von Bachmann <philippvonbachmann@web.de>
2022-05-12 11:55:53 +0200

    Added model loading to smooth labels

--- train_label_smoothing.py
+++ train_label_smoothing.py
@@ -9,6 +9,8 @@ from torch.autograd import Variable
 from PIL import Image
 import torch
 from tqdm import tqdm
+import pandas as pd
+import os
 
 from models import Generator
 from models import Discriminator
@@ -17,7 +19,6 @@ from utils import LambdaLR
 # from utils import Logger
 from utils import weights_init_normal
 from datasets import ImageDataset, RamImageDataset
-import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--epoch', type=int, default=0, help='starting epoch')
@@ -33,6 +34,7 @@ parser.add_argument('--cuda', action='store_true', help='use GPU computation')
 parser.add_argument('--n_cpu', type=int, default=8, help='number of cpu threads to use during batch generation')
 parser.add_argument('--split', type=float, default=1, help='Train/Val split')
 parser.add_argument('--output_dir', type=str, default="./output/", help='The output directory to save the model to')
+parser.add_argument('--load_dir', type=str, default=None, help='If provided, loads the model from that directory')
 opt = parser.parse_args()
 print(opt)
 
@@ -45,6 +47,18 @@ netG_A2B = Generator(opt.input_nc, opt.output_nc)
 netG_B2A = Generator(opt.output_nc, opt.input_nc)
 netD_A = Discriminator(opt.input_nc)
 netD_B = Discriminator(opt.output_nc)
+
+# load variables
+if opt.load_dir:
+    print("Loading model")
+    netG_A2B.load_state_dict(torch.load(os.path.join(opt.output_dir, 'netG_A2B.pth')))
+    netG_B2A.load_state_dict(torch.load(os.path.join(opt.output_dir, 'netG_B2A.pth')))
+    netD_A.load_state_dict(torch.load(os.path.join(opt.output_dir, 'netD_A.pth')))
+    netD_B.load_state_dict(torch.load(os.path.join(opt.output_dir, 'netD_B.pth')))
+
+# check if output dir exists
+if not os.path.exists(opt.output_dir):
+    os.makedirs(opt.output_dir)
 
 if opt.cuda:
     netG_A2B.cuda()
@@ -77,8 +91,8 @@ Tensor = torch.cuda.FloatTensor if opt.cuda else torch.Tensor
 input_A = Tensor(opt.batchSize, opt.input_nc, opt.size, opt.size)
 input_B = Tensor(opt.batchSize, opt.output_nc, opt.size, opt.size)
 smooth = 0.9
-target_real = Variable(Tensor(opt.batchSize).fill_(smooth), requires_grad=False)
-target_fake = Variable(Tensor(opt.batchSize).fill_(1-smooth), requires_grad=False)
+target_real = torch.full((opt.batchSize, 1), fill_value=smooth).float()
+target_fake = torch.full_like(target_real, fill_value=1 - smooth).float()
 
 fake_A_buffer = ReplayBuffer()
 fake_B_buffer = ReplayBuffer()
@@ -101,6 +115,9 @@ dataloader = DataLoader(train_set,
 
 ###### Training ######
 for epoch in range(opt.epoch, opt.n_epochs):
+    print("Epoch", epoch)
+    epoch_hist = pd.DataFrame(columns=['epoch', 'loss_G', 'loss_G_identity', 'loss_G_GAN',
+                        'loss_G_cycle', 'loss_D'])
     for batch in tqdm(dataloader):
         # Set model input
         real_A = Variable(input_A.copy_(batch['A']))
@@ -178,10 +195,15 @@ for epoch in range(opt.epoch, opt.n_epochs):
         optimizer_D_B.step()
         ###################################
 
-        # Progress report (http://localhost:8097)
-        # logger.log({'loss_G': loss_G, 'loss_G_identity': (loss_identity_A + loss_identity_B), 'loss_G_GAN': (loss_GAN_A2B + loss_GAN_B2A),
-        #             'loss_G_cycle': (loss_cycle_ABA + loss_cycle_BAB), 'loss_D': (loss_D_A + loss_D_B)}, 
-        #             images={'real_A': real_A, 'real_B': real_B, 'fake_A': fake_A, 'fake_B': fake_B})
+        with torch.no_grad():
+            batch_hist = {'epoch': torch.tensor([epoch]),'loss_G': loss_G, 'loss_G_identity': (loss_identity_A + loss_identity_B), 'loss_G_GAN': (loss_GAN_A2B + loss_GAN_B2A), 'loss_G_cycle': (loss_cycle_ABA + loss_cycle_BAB), 'loss_D': (loss_D_A + loss_D_B)}
+            batch_hist = {key: [round(value.detach().cpu().item(), 4)] for key, value in batch_hist.items()}
+            epoch_hist = epoch_hist.append(pd.DataFrame(batch_hist))
+
+    # after epoch, save hist to file
+    output_filepath = os.path.join(opt.output_dir, "history.csv")
+    epoch_hist = pd.DataFrame(epoch_hist.mean(axis=0)).transpose()
+    epoch_hist.to_csv(output_filepath, index=False, mode="a", header=not os.path.exists(output_filepath))
 
     # Update learning rates
     lr_scheduler_G.step()
