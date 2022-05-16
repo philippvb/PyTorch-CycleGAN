1e277606c8a09a172abb1d42681eee4fb2d6fc2a
Richards <richards.1779@gmail.com>
2022-05-16 03:51:42 +0200

    Momentum Optimizer0.2

--- train_richards.py
+++ train_richards.py
@@ -67,10 +67,10 @@ criterion_identity = torch.nn.L1Loss()
 #                                lr=opt.lr, betas=(0.5, 0.999))
 #optimizer_D_A = torch.optim.Adam(netD_A.parameters(), lr=opt.lr, betas=(0.9, 0.999))
 #optimizer_D_B = torch.optim.Adam(netD_B.parameters(), lr=opt.lr, betas=(0.9, 0.999))
-optimizer_G = torch.optim.Adam(itertools.chain(netG_A2B.parameters(), netG_B2A.parameters()),
+optimizer_G = torch.optim.SGD(itertools.chain(netG_A2B.parameters(), netG_B2A.parameters()),
                                 lr=opt.lr, momentum=0.9)
-optimizer_D_A = torch.optim.Adam(netD_A.parameters(), lr=opt.lr, momentum=0.9)
-optimizer_D_B = torch.optim.Adam(netD_B.parameters(), lr=opt.lr, momentum=0.9)
+optimizer_D_A = torch.optim.SGD(netD_A.parameters(), lr=opt.lr, momentum=0.9)
+optimizer_D_B = torch.optim.SGD(netD_B.parameters(), lr=opt.lr, momentum=0.9)
 
 
 lr_scheduler_G = torch.optim.lr_scheduler.LambdaLR(optimizer_G, lr_lambda=LambdaLR(opt.n_epochs, opt.epoch, opt.decay_epoch).step)
